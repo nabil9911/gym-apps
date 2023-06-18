a13ed43d52c9d7202a2a331d5fb6068ee40d22cf
nabil99 <gilanggokilss19@gmail.com>
2023-06-18 12:15:15 +0700

    commit ketiga

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -16,7 +16,7 @@ type Props = {
 const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("min-width:1060px");
   return (
-    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+    <section id="home" className=" gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* Image And Main Header */}
       <motion.div
         className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
